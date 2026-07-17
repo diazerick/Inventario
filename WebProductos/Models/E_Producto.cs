7ced246a82cf70b8674bb340b79ef2041cf9b89e
Erick <diazf.erick@gmail.com>
2026-07-17 11:35:16 -0600

    Agrego propiedades gerente a E_producto

--- WebProductos/Models/E_Producto.cs
+++ WebProductos/Models/E_Producto.cs
@@ -3,12 +3,23 @@
     public class E_Producto
     {
         //Propiedades simples
-        public int IdProducto { get; set; }
+        public int IdGerente { get; set; }
+        public DateTime FechaIngreso { get; set; }
         public string Descripcion { get; set; }
         public decimal Precio { get; set; }
-        public DateTime FechaIngreso { get; set; }
-        public bool Disponible { get; set; }
         public int PrecioDescuento { get; set; }
+        public string Gerente { get; set; }
+        public int IdProducto { get; set; }
+        public bool Disponible { get; set; }
+        public int IdSucursal { get; set; }
+       
+        public DateTime FechaCaducidad
+        {
+            get
+            {
+                return FechaIngreso.AddMonths(2);
+            }
+        }
 
         //Propiedades full o de solo de lectura
         public string DisponibleTexto
@@ -19,14 +30,6 @@
                     return "Si";
                 else
                     return "No";
-            }
-        }
-
-        public DateTime FechaCaducidad
-        {
-            get
-            {
-                return FechaIngreso.AddMonths(2);
             }
         }
     }
