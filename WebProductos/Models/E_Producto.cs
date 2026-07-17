c3e6921821069ebdf907d6a925cb2513f8afce9a
Erick <diazf.erick@gmail.com>
2026-07-17 11:34:19 -0600

    Agrego propiedades a E_producto

--- WebProductos/Models/E_Producto.cs
+++ WebProductos/Models/E_Producto.cs
@@ -4,11 +4,15 @@
     {
         //Propiedades simples
         public int IdProducto { get; set; }
+        public int IdVendedor { get; set; }
         public string Descripcion { get; set; }
         public decimal Precio { get; set; }
         public DateTime FechaIngreso { get; set; }
         public bool Disponible { get; set; }
+        public string NombreVendedor { get; set; }
         public int PrecioDescuento { get; set; }
+
+        public string NombreSucursal { get; set; }
 
         //Propiedades full o de solo de lectura
         public string DisponibleTexto
